3613530ad41cba9e816d98b27b4c8e1e897785dc
KIC_WEB <webadmin@kicsv.org>
2023-02-28 17:44:05 -0800

    Add board page layout

---
+++ assets/js/firebase.js
@@ -0,0 +1,24 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+// import {
+//   getFirestore,
+//   collection,
+//   getDocs,
+// } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-MHHv4MrHpeHHqTntiGUQ_OhSfFQcPMs",
+//   authDomain: "kic-home-board.firebaseapp.com",
+//   projectId: "kic-home-board",
+//   storageBucket: "kic-home-board.appspot.com",
+//   messagingSenderId: "609107485683",
+//   appId: "1:609107485683:web:d1c116f3ed6185bb922886",
+// };
+
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// const querySnapshot = await getDocs(collection(db, "posts"));
+// querySnapshot.forEach((doc) => {
+//   console.log(`${doc.id} => ${doc.data()}`);
+// });
